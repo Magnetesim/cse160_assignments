6615b2f39c43941f2bf88f922b421bb1d6a79d10
Myles Hallett <mthallet@ucsc.edu>
2026-04-05 22:02:43 -0700

    part 2 done

--- asgn0/asg0.js
+++ asgn0/asg0.js
@@ -10,7 +10,19 @@ function main() {
   // Get the rendering context for 2DCG
   var ctx = canvas.getContext('2d');
 
-  // Draw a blue rectangle
-  ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set color to blue
-  ctx.fillRect(120, 10, 150, 150);        // Fill a rectangle with the color
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, 400, 400); // setting background as black before doing stuff
+
+  v1 = new Vector3([2.25, 2.25, 0]);
+
+  drawVector(ctx, v1, "red")
+}
+
+
+function drawVector(ctx, v, color) {
+  ctx.beginPath();
+  ctx.strokeStyle = color;
+  ctx.moveTo(200, 200);
+  ctx.lineTo(200 + v.elements[0] * 20, 200 - v.elements[1] * 20);
+  ctx.stroke();
 }
